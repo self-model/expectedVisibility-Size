4ee0dd07b71522ce9bd803c4c1021728deccdb96
Maya Schipper <schippermaya@gmail.com>
2024-08-10 09:58:39 +0100

    model fitting script

--- modelling/modelFitting/fitSize/run_fit_parameters.sh
+++ modelling/modelFitting/fitSize/run_fit_parameters.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=48
-#SBATCH --job-name=fit_parameters
+#SBATCH --job-name=fit_parameters_size
 #SBATCH --partition=short
 #SBATCH --time=8:00:00
 #SBATCH --mail-type=BEGIN,END
-#SBATCH --mail-user=alls0672@ox.ac.uk
+#SBATCH --mail-user=lady7504@ox.ac.uk
 
 module purge
 module load MATLAB/R2023a
-matlab -nodisplay -nosplash < fit_parameters.m > run.log
+matlab -nodisplay -nosplash < fit_parameters_size.m > run.log
 continue
 
