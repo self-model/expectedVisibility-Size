3a1f45cc0c87f9f279a65bbb9a338c060c2b375c
Maya Schipper <schippermaya@gmail.com>
2024-08-16 23:42:59 +0100

    model-fitting results occlusion with intersected subj_ids df

--- modelling/modelFitting/fitOcclusion/run_fit_parameters.sh
+++ modelling/modelFitting/fitOcclusion/run_fit_parameters.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=48
-#SBATCH --job-name=fit_parameters
+#SBATCH --job-name=fit_parameters_occlusion
 #SBATCH --partition=short
 #SBATCH --time=10:00:00
 #SBATCH --mail-type=BEGIN,END
@@ -9,6 +9,6 @@
 
 module purge
 module load MATLAB/R2023a
-matlab -nodisplay -nosplash < fit_parameters_occlusion.m > run.log
+matlab -nodisplay -nosplash < fit_parameters_occlusion.m > run_intersect.log
 continue
 
