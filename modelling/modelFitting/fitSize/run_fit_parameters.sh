4599de6741e810205240a4cf46362cb02693f1a2
Maya Schipper <schippermaya@gmail.com>
2024-08-17 21:02:58 +0100

    model-fitting output size

--- modelling/modelFitting/fitSize/run_fit_parameters.sh
+++ modelling/modelFitting/fitSize/run_fit_parameters.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=48
-#SBATCH --job-name=fit_parameters_size
+#SBATCH --job-name=fit_parameters_size_intersect
 #SBATCH --partition=short
-#SBATCH --time=1:00:00
+#SBATCH --time=10:00:00
 #SBATCH --mail-type=BEGIN,END
 #SBATCH --mail-user=lady7504@ox.ac.uk
 
 module purge
 module load MATLAB/R2023a
-matlab -nodisplay -nosplash < fit_parameters_size.m > run.log
+matlab -nodisplay -nosplash < fit_parameters_size_intersect.m > run_intersect.log
 continue
 
